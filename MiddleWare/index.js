17b67ab5d9e2c24def6ba062e1863465c66479fc
CodeCommandBD <shantokumar22@gmail.com>
2025-07-23 16:08:51 +0600

    update

--- MiddleWare/index.js
+++ MiddleWare/index.js
@@ -4,8 +4,21 @@ const app = express()
 
 const port = 3000
 
+const myMiddle = (req, res, next) => {
+    
+    req.current = new Date(Date.now())
+    next()
+}
+
+app.use(myMiddle)
+
 app.get('/', (req, res)=>{
-    res.send('hello')
+    res.send('hello' + req.current)
+    
+})
+app.get('/about', (req, res)=>{
+    res.send('hello' + req.current)
+    
 })
 
 
